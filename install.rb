3cdbf5d6e54ac66deab8b5a040ba995311d50aea
Michael Berkovich <michael@geni.com>
2010-05-21 19:25:36 -0700

    trying to make the install script install everything

--- install.rb
+++ install.rb
@@ -1,5 +1,8 @@
 # Install hook code here
 
+sh "script/plugin install git://github.com/mislav/will_paginate.git"
+sh "script/plugin install git://github.com/berk/will_filter.git"
+sh "rake will_filter:sync"  
 sh "rake tr8n:sync"  
 sh "rake db:migrate" 
 sh "rake tr8n:init"
