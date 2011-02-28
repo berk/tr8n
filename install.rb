c17322cb1ac7eb1d9f31d9165e73e39e4db2db1f
Mariusz Droździel <mdrozdziel@gmail.com>
2011-02-28 14:37:29 +0100

    fix install.rb sh

--- install.rb
+++ install.rb
@@ -23,9 +23,10 @@
 
 # Install hook code here
 
-sh "script/plugin install git://github.com/mislav/will_paginate.git"
-sh "script/plugin install git://github.com/berk/will_filter.git"
-sh "rake will_filter:sync"  
-sh "rake tr8n:sync"  
-sh "rake db:migrate" 
-sh "rake tr8n:init"
+%x[ rails plugin install git://github.com/mislav/will_paginate.git ]
+%x[ rails plugin install git://github.com/berk/will_filter.git ]
+%x[ rake will_filter:sync ]
+%x[ rake tr8n:sync ]
+%x[ rake db:migrate ]
+%x[ rake tr8n:init ]
+
