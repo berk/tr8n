e139c191404e31d74a699186def8e165d152af5c
Michael Berkovich <michael@geni.com>
2012-04-12 12:32:24 -0700

    fixed keyboard image location

--- app/javascripts/src/ext/keyboard_1_49.js
+++ app/javascripts/src/ext/keyboard_1_49.js
@@ -75,7 +75,7 @@ var VKI_default_layout = VKI_default_layout || "US International";
   this.VKI_size = 2;  // Default keyboard size (1-5)
   this.VKI_sizeAdj = true;  // Allow user to adjust keyboard size
   this.VKI_clearPasswords = false;  // Clear password fields on focus
-  this.VKI_imageURI = "/assets/tr8n/keyboard.png";  // If empty string, use imageless mode
+  this.VKI_imageURI = "/tr8n/images/keyboard.png";  // If empty string, use imageless mode
   this.VKI_clickless = 0;  // 0 = disabled, > 0 = delay in ms
   this.VKI_activeTab = 0;  // Tab moves to next: 1 = element, 2 = keyboard enabled element
   this.VKI_enterSubmit = true;  // Submit forms when Enter is pressed
