1d14067b9e81648747d83b45f4a349a1f8eb0f11
Drew Chandler <drew@kongregate.com>
2012-07-25 14:48:08 -0700

    Don't clash with rails_xss

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -45,14 +45,16 @@ class String
   def tr8n_translated?
     @tr8n_translated
   end
-  
-  def html_safe
-    @html_safe = true
-    self
-  end
-  
-  def html_safe?
-    @html_safe
+
+  unless method_defined?(:html_safe)
+    def html_safe
+      @html_safe = true
+      self
+    end
+
+    def html_safe?
+      @html_safe
+    end
   end
   
 end
