d8ad7483edbc3ea067fb8c431e16ea9cb10c1d9e
Chipairon <chipairon@fake.server>
2013-01-11 08:16:02 +0100

    Fix change language with shadowed locale When trying to change language from tr8n overlay it fails because locale from params takes precedence over selected language from the form. This filter checks the shadowing and makes the switch.

--- app/controllers/tr8n/language_controller.rb
+++ app/controllers/tr8n/language_controller.rb
@@ -27,7 +27,8 @@ module Tr8n
     before_filter :validate_guest_user, :except => [:select, :switch, :translator_splash_screen, :toggle_inline_translations, :change, :table]
     before_filter :validate_current_translator, :except => [:select, :switch, :translator_splash_screen, :toggle_inline_translations, :change, :table]
     before_filter :validate_language_management, :only => [:index]
-    
+    before_filter :validate_locale_is_not_shadowed
+
     # for ssl access to the translator - using ssl_requirement plugin  
     ssl_allowed :translator, :select, :lists, :switch, :remove  if respond_to?(:ssl_allowed)
 
@@ -35,6 +36,31 @@ module Tr8n
       @rules = rules_by_dependency(tr8n_current_language.rules)
       @cases = tr8n_current_language.cases
       @fallback_language = (tr8n_current_language.fallback_language || tr8n_default_language)
+    end
+
+    def validate_locale_is_not_shadowed
+      mismatch = false
+      if request.env["rack.request.form_hash"] and request.env["rack.request.form_hash"]["locale"]
+        if params[:locale] != request.env["rack.request.form_hash"]["locale"]
+          mismatch = true
+          puts "Locale mismatch found in tr8n controller."
+          puts "URL says: #{params[:locale]}"
+          puts "Form says: #{request.env["rack.request.form_hash"]["locale"]}"
+          puts "changing it to form locale."
+          params[:locale] = request.env["rack.request.form_hash"]["locale"]
+        end
+      end
+      if params["action"] == "switch" or mismatch
+        target_url = params["source_url"]
+        params[:locale] = request[:locale]
+        url_parts = params["source_url"].split("/")
+        if url_parts.count > 3 and request[:locale]
+          url_parts[3] = request[:locale]
+          target_url = url_parts.join("/")
+        end
+        request["source_url"] = target_url
+        redirect_to_source and return
+      end
     end
 
     def update_language_section
@@ -45,14 +71,14 @@ module Tr8n
       unless request.post?
         return render(:partial => params[:section], :locals => {:mode => params[:mode].to_sym})
       end
-    
+
       @error_msg = validate_language
       if @error_msg
         return render(:partial => params[:section], :locals => {:mode => params[:mode].to_sym})
       end
 
       tr8n_current_language.update_attributes(params[:language])
-    
+
       if params[:section] == 'grammar'
         old_rule_ids = tr8n_current_language.rules.collect{|rule| rule.id}
         parse_language_rules.each do |rule|
@@ -66,16 +92,16 @@ module Tr8n
           rule = Tr8n::LanguageRule.find(id)
           rule.destroy_with_log!(tr8n_current_translator)
         end
-      
+
         Tr8n::Cache.delete("language_rules_#{tr8n_current_language.id}") 
       end
-    
+
       if params[:section] == 'language_cases'
         # remove old cases
         tr8n_current_language.cases.each do |lcase|
           lcase.destroy
         end
-      
+
         # create new cases
         parse_language_cases.each do |lcase|
           lcase.language = tr8n_current_language
@@ -84,20 +110,20 @@ module Tr8n
 
         Tr8n::Cache.delete("language_cases_#{tr8n_current_language.id}") 
       end
-    
+
       tr8n_current_language.reload
-    
+
       @rules = rules_by_dependency(tr8n_current_language.rules)
       @cases = tr8n_current_language.cases
       @fallback_language = (tr8n_current_language.fallback_language || tr8n_default_language)
 
       render(:partial => params[:section], :locals => {:mode => :view})
     end
-  
+
     # ajax method for updating language rules in edit mode
     def update_rules
       @rules = rules_by_dependency(parse_language_rules)
-    
+
       unless params[:rule_action]
         return render(:partial => "edit_rules")
       end
@@ -111,18 +137,18 @@ module Tr8n
         cls = Tr8n::Config.language_rule_dependencies[params[:rule_type]]
         @rules[cls.dependency].delete_at(position)
       end
-    
+
       render :partial => "edit_rules"      
     end
-  
+
     # ajax method for updating language cases in edit mode
     def update_language_cases
       @cases = parse_language_cases
-    
+
       unless params[:case_action]
         return render(:partial => "edit_cases")
       end
-  
+
       if params[:case_action].index("add_at")
         position = params[:case_action].split("_").last.to_i
         @cases.insert(position, Tr8n::LanguageCase.new(:language => tr8n_current_language))
@@ -132,10 +158,10 @@ module Tr8n
       elsif params[:case_action].index("clear_all")
         @cases = []
       end
-    
+
       render :partial => "edit_language_cases"      
     end
-  
+
     # ajax method for updating language case rules in edit mode
     def update_language_case_rules
       cases = parse_language_cases
@@ -146,7 +172,7 @@ module Tr8n
         position = params[:case_action].split("_").last.to_i
         rule_data = params[:edit_rule].merge(:language => tr8n_current_language)
         lcase.language_case_rules.insert(position, Tr8n::LanguageCaseRule.new(rule_data))
-      
+
       elsif params[:case_action].index("update_rule_at")
         position = params[:case_action].split("_").last.to_i
         rule_data = params[:edit_rule].merge(:language => tr8n_current_language)
@@ -163,25 +189,25 @@ module Tr8n
         temp_node = lcase.language_case_rules[position+1]
         lcase.language_case_rules[position+1] = lcase.language_case_rules[position]
         lcase.language_case_rules[position] = temp_node              
-              
+
       elsif params[:case_action].index("delete_rule_at")
         position = params[:case_action].split("_").last.to_i
         lcase.language_case_rules.delete_at(position)
-      
+
       elsif params[:case_action].index("clear_all")
         lcase.language_case_rules = []
-      
+
       end
-    
+
       render(:partial => "edit_language_case_rules", :locals => {:lcase => lcase, :case_index => case_index})
     end
-  
-  
+
+
     # language selector window
     def select
       @inline_translations_allowed = false
       @inline_translations_enabled = false
-    
+
       if tr8n_current_user_is_translator? 
         unless tr8n_current_translator.blocked?
           @inline_translations_allowed = true
@@ -190,22 +216,22 @@ module Tr8n
       else
         @inline_translations_allowed = Tr8n::Config.open_registration_mode?
       end
-    
+
       @inline_translations_allowed = true if tr8n_current_user_is_admin?
-    
+
       @source_url = request.env['HTTP_REFERER']
       @source_url.gsub!("locale", "previous_locale") if @source_url
-    
+
       @all_languages = Tr8n::Language.enabled_languages
       @user_languages = Tr8n::LanguageUser.languages_for(tr8n_current_user) unless tr8n_current_user_is_guest?
-    
+
       if params[:lightbox]
         render :partial => "select"
       else
         render :layout => false
       end      
     end
-  
+
     # language selector management functions
     def lists
       if request.post? 
@@ -214,18 +240,18 @@ module Tr8n
           lu.destroy
         end
       end
-    
+
       @all_languages = Tr8n::Language.enabled_languages
       @user_languages = Tr8n::LanguageUser.languages_for(tr8n_current_user)
       render(:partial => "lists")  
     end
-  
+
     def remove
       lu = Tr8n::LanguageUser.find(:first, :conditions => ["language_id = ? and user_id = ?", params[:language_id].to_i, tr8n_current_user.id])
       lu.destroy if lu
       redirect_to_source
     end
-  
+
     def enable_inline_translations
       tr8n_current_translator.enable_inline_translations!
       redirect_to_source
@@ -239,9 +265,9 @@ module Tr8n
     # language selector processor
     def switch
       language_action = params[:language_action]
-    
+
       return redirect_to_source if tr8n_current_user_is_guest?
-    
+
       if tr8n_current_user_is_translator? # translator mode
         if language_action == "toggle_inline_mode"
           if tr8n_current_translator.enable_inline_translations?
@@ -250,7 +276,7 @@ module Tr8n
             language_action = "enable_inline_mode"
           end
         end
-      
+
         if language_action == "enable_inline_mode"
           tr8n_current_translator.enable_inline_translations!
         elsif language_action == "disable_inline_mode"
@@ -265,7 +291,7 @@ module Tr8n
       elsif language_action == "enable_inline_mode" or language_action == "toggle_inline_mode" # non-translator mode
         Tr8n::Translator.register.enable_inline_translations!
       end
-    
+
       redirect_to_source
     end
 
@@ -296,7 +322,7 @@ module Tr8n
       end
       render(:layout => false)
     end
-    
+
     def translator_splash_screen
       render :layout => false
     end
@@ -304,41 +330,41 @@ module Tr8n
     def table
       @source_url = params[:source_url] || request.env['HTTP_REFERER']
     end
-  
+
     def lb_language_case_rule
       @case = Tr8n::LanguageCase.new(params[:case])
       @rule_index = params[:rule_index]
-    
+
       if params[:case_action].index("add_rule_at")
         @rule = Tr8n::LanguageCaseRule.new(:definition => {})
         @new_rule = true
       else
         @rule = Tr8n::LanguageCaseRule.new(params[:rule])
       end
-    
+
       render :layout => false
     end
-  
-  private
+
+    private
 
     # parse with safety - we don't want to disconnect existing translations from those rules
     def parse_language_rules
       rulz = []
       return rulz unless params[:rules]
-    
+
       Tr8n::Config.language_rule_classes.each do |cls|
         next unless params[:rules][cls.dependency]
         index = 0    
         while params[:rules][cls.dependency]["#{index}"]
           rule_params = params[:rules][cls.dependency]["#{index}"]
           rule_definition = params[:rules][cls.dependency]["#{index}"][:definition]
-        
+
           if rule_params.delete(:reset_values) == "true"
             rule_definition = {}
           end
-  
+
           rule_id = rule_params[:id]
-        
+
           if rule_id.blank?
             rulz << cls.new(:definition => rule_definition)
           else
@@ -349,22 +375,22 @@ module Tr8n
           end
           index += 1
         end
-    
+
       end 
-    
+
       rulz
     end
 
     def parse_language_cases
       cases = []
       return cases unless params[:cases]
-    
+
       case_index = 0  
       while params[:cases]["#{case_index}"]
         case_rules = params[:cases]["#{case_index}"].delete("rules")
         lcase = Tr8n::LanguageCase.new(params[:cases]["#{case_index}"])
         rules = []
-      
+
         if case_rules
           rule_index = 0
           while case_rules["#{rule_index}"]
@@ -373,12 +399,12 @@ module Tr8n
             rule_index += 1
           end
         end
-      
+
         lcase.language_case_rules = rules      
         cases << lcase
         case_index += 1
       end
-    
+
       cases
     end
 
@@ -391,7 +417,7 @@ module Tr8n
       rules.each{|rule| (types[rule.class.dependency] ||= []) << rule}
       types
     end
-  
+
     def case_index
       (params[:case_index] || 0).to_i
     end
